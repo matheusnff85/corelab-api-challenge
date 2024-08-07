7eb43fe0f76ce9467215e34fb9e69fb8d84cbccf
matheusnff85 <matheus.marinhodsp@gmail.com>
2024-08-07 18:07:28 -0300

    feat: adiciona swagger e configurações do zod ao arquivo server.ts, adiciona rota de criação de nova task, e configuração do prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,37 @@
 import fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+
+import { createTask } from './routes/create-task';
 
 const app = fastify();
 
-app.get('/', () => {
-  return 'Hello World';
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, { origin: '*' });
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'Corelab API Challenge',
+      description: 'Rotas para a API do desafio da Corelab',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
 });
+app.register(fastifySwaggerUI, { routePrefix: '/docs' });
+
+app.register(createTask);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running on port 3333 🚀');
